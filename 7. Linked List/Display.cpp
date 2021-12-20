b952a980204b4aa0f8c5a2f5139e97e63bd42278
satymsb <68340284+satymsb@users.noreply.github.com>
2021-12-20 12:43:59 +0530

    Update Display.cpp

--- 7. Linked List/Display.cpp
+++ 7. Linked List/Display.cpp
@@ -1,144 +1,164 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Node{
-    public: 
-
+class Node
+{
+public:
     int Data;
     Node *Next;
-}*first=NULL;
- 
+} *first = NULL,*second=NULL,*third=NULL;
+
 void Create(int A[], int n)
 {
     int i;
     Node *t, *last;
-    first=new Node;
-    first->Data=A[0];
-    first->Next=NULL;
-    last=first;
+    first = new Node;
+    first->Data = A[0];
+    first->Next = NULL;
+    last = first;
 
-    for(i=1; i<n; i++)
+    for (i = 1; i < n; i++)
     {
-        t=new Node;
-        t->Data=A[i];
-        t->Next=NULL;
-        last->Next=t;
-        last=t;
+        t = new Node;
+        t->Data = A[i];
+        t->Next = NULL;
+        last->Next = t;
+        last = t;
+    }
+}
+void Create2(int A[], int n)
+{
+    int i;
+    Node *t, *last;
+    second = new Node;
+    second->Data = A[0];
+    second->Next = NULL;
+    last = second;
+
+    for (i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->Data = A[i];
+        t->Next = NULL;
+        last->Next = t;
+        last = t;
     }
 }
 void Display(Node *p)
 {
-    
-    while(p!=NULL)
+
+    while (p != NULL)
     {
-        cout<<p->Data<<" ->";
-        p=p->Next;
-        
+        cout << p->Data << " ->";
+        p = p->Next;
     }
-    cout<<endl;
+    cout << endl;
 }
-void RDisplay(Node *p) //Recursive function
+void RDisplay(Node *p) // Recursive function
 {
-    if(p!=NULL)
+    if (p != NULL)
     {
-        cout<<p->Data<<"->";
+        cout << p->Data << "->";
         RDisplay(p->Next);
     }
 }
 int Count(Node *p)
 {
-    int count=0;
-    while(p)
+    int count = 0;
+    while (p)
     {
         count++;
-        p=p->Next;
+        p = p->Next;
     }
     return count;
 }
 int RCount(Node *p)
 {
-    if(p)
+    if (p)
     {
-        return RCount(p->Next)+1;
+        return RCount(p->Next) + 1;
     }
     else
         return 0;
 }
 int Sum(Node *p)
 {
-    int sum=0;
-    while(p!=NULL)
+    int sum = 0;
+    while (p != NULL)
     {
-        sum+=p->Data;
-        p=p->Next;
+        sum += p->Data;
+        p = p->Next;
     }
     return sum;
 }
 int RSum(Node *p)
 {
-    if(p)
+    if (p)
     {
-       return RSum(p->Next)+p->Data;
+        return RSum(p->Next) + p->Data;
     }
-    else 
+    else
         return 0;
 }
 int Max(Node *p)
 {
-    int Max=INT16_MIN; //max=-32768 
-    while(p)
+    int Max = INT16_MIN; // max=-32768
+    while (p)
     {
-        if(p->Data>Max)
+        if (p->Data > Max)
         {
-            Max=p->Data;
+            Max = p->Data;
         }
-         p=p->Next;   
+        p = p->Next;
     }
     return Max;
 }
 int RMax(Node *p)
 {
-    int x=0;
-    if(p==0)
+    int x = 0;
+    if (p == 0)
         return INT16_MIN;
-    x=RMax(p->Next);
+    x = RMax(p->Next);
 
-    return x>p->Data?x:p->Data; //turnary operator
-
+    return x > p->Data ? x : p->Data; // turnary operator
 }
 int Min(Node *p)
 {
-    int Min=INT16_MAX; //Min=32767
-    while(p)
+    int Min = INT16_MAX; // Min=32767
+    while (p)
     {
-        if(Min>p->Data)
+        if (Min > p->Data)
         {
-            Min=p->Data;
+            Min = p->Data;
         }
-        p=p->Next;
+        p = p->Next;
     }
     return Min;
 }
-Node* Search(Node *p, int key)
+Node *Search(Node *p, int key)
 {
-    Node *q=NULL;       //This is move to head method
-    while(p!=NULL)
+    Node *q = NULL; // This is move to head method
+    while (p != NULL)
     {
-        if(key ==p->Data)
+        if (key == p->Data)
         {
-            q->Next=p->Next;
-            p->Next=first;
-            first=p;
+            if (p == first)
+            {
+                return first;
+            }
+            q->Next = p->Next;
+            p->Next = first;
+            first = p;
             return (p);
         }
-        q=p;
-        p=p->Next;
+        q = p;
+        p = p->Next;
     }
     return 0;
 }
-Node* RSearch(Node *p, int key)
+Node *RSearch(Node *p, int key)
 {
-    if(p==NULL)
+    if (p == NULL)
     {
         return 0;
     }
@@ -146,58 +166,243 @@ Node* RSearch(Node *p, int key)
     // {
     //     return p;
     // }
-    // else    
+    // else
     // {
     //     RSearch(p->Next, key);  this method will always give warning
     // }
-    return key==p->Data?p:RSearch(p->Next,key); //turnary operator
+    return key == p->Data ? p : RSearch(p->Next, key); // turnary operator
 }
 void Insert(int index, int x)
 {
     Node *p, *t;
-    if(index < 0 || index > Count(p) )
+    if (index < 0 || index > Count(p))
         return;
-    if(index==0)
+    if (index == 0)
     {
-        t->Data=x;
-        t->Next=first;
-        first=t;
+        t->Data = x;
+        t->Next = first;
+        first = t;
     }
-    else if(index > 0)
+    else if (index > 0)
     {
-        p=first;
-        for(int i=0;i<index-1 && p ; i++)
-           p=p->Next;
-        
-            if(p)
-            {   
-                t= new Node;
-                t->Data=x;
-                t->Next=p->Next;
-                p->Next=t;             
-            }
+        p = first;
+        for (int i = 0; i < index - 1 && p; i++)
+            p = p->Next;
+
+        if (p)
+        {
+            t = new Node;
+            t->Data = x;
+            t->Next = p->Next;
+            p->Next = t;
+        }
     }
 }
-int main(){
-int A[]={3,5,35};
+void SortedInsert(Node *p, int x) // For sorted linked list at sorted position
+{
+    Node *t, *q = NULL;
+    t = new Node;
+    t->Data = x;
+    t->Next = NULL;
 
-Create(A, 3);
-Display(first);
+    if (first == NULL)
+    {
+        first = t;
+    }
+    else
+    {
+        while (p && p->Data < x)
+        {
+            q = p;
+            p = p->Next;
+        }
+        if (p == first)
+        {
+            t->Next = first;
+            first = t;
+        }
+        else // here x is greater than p->Data
+        {
+            t->Next = q->Next;
+            q->Next = t;
+        }
+    }
+}
+int Delete(Node *p, int index)
+{
+    Node *q = new Node;
+    int x = -1, i;
+    if (index <= 0 || index > Count(p))
+    {
+        return -1;
+    }
+    if (index == 1)
+    {
+        p = first;
+        first = first->Next;
+        x = p->Data;
+        delete p;
+        return x;
+    }
+    else
+    {
+        p = first;
+        q = NULL;
+        for (i = 0; i < index - 1 && p; i++)
+        {
+            q = p;
+            p = p->Next;
+        }
+        if (p)
+        {
+            q->Next = p->Next;
+            x = p->Data;
+            delete p;
+        }
+        return x;
+    }
+}
+bool Sorted(Node *p)
+{
+    int x = INT16_MIN;
+    while (p)
+    {
+        if (p->Data > x)
+        {
+            x = p->Data;
+            p = p->Next;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    return true;
+}
+void RemoveDuplicate(Node *p)
+{
+    p=first;
+    Node * q=first->Next;
 
-// cout<<RCount(first)<<" number of nodes are present \n";
-// cout<<Sum(first)<<" is the Addition of elemets ";    
+    while(q)
+    {
+        if(p->Data!=q->Data)
+        {
+            p=q;
+            q=q->Next;
+        } 
+        else
+        {
+            p->Next=q->Next;
+            delete q;
+            q=p->Next;
+        }
+    }
+}
+void Reverse(Node *p) //Reversing elements using array(a bit sloppy)
+{
+    int *A;
+    A= new int  [Count(p)];
+    int i=0;
+    while(p)
+    {
+        A[i]=p->Data;
+        p=p->Next;
+        i++;
+    }
+    p=first;
+    i--;
+    while(p)
+    {
+        p->Data=A[i];
+        p=p->Next;
+        i--;
+    }
+}
+void Reverse1(Node *p) //Sliding process (preferable)
+{
+    
+    Node *q=NULL,*r=NULL;
+    while(p)
+    {
+        r=q;
+        q=p;
+        p=p->Next;
+        q->Next=r;
+    }
+    first=q;
+}
+void Reverse2(Node *q, Node *p)
+{
+    if(p)
+    {
+        Reverse2(p,p->Next);
+        p->Next=q;
+    }
+    else
+        first=q;
+}
+void Concatenate(Node *p, Node *q)
+{
+    third=p;
+    while(p->Next)
+    {
+        p=p->Next;
+    }
+    p->Next=q;
+    q=NULL;
+}
+void Merge(Node *p, Node *q)
+{
+    Node *last;
+    if(p->Data < q->Data)
+    {
+        third=last=p;
+        p=p->Next;
+        third->Next=NULL;
+    }
+    else
+    {
+        third=last=q;
+        q=q->Next;
+        third->Next=NULL;
+    }
+    while(p && q)
+    {
+        if(p->Data < q->Data)
+        {
+            last->Next=p;
+            last=p;
+            p=p->Next;
+            last->Next=NULL;
+        }
+        else
+        {
+            last->Next=q;
+            last=q;
+            q=q->Next;
+            last->Next=NULL;
+        }
+    }
+        if(p)
+        {
+            last->Next=p;
+        }
+        else
+        {
+            last->Next=q;
+        }
+}
+int main()
+{
+    int A[] = {1, 2, 6, 8, 15};
+    int B[] = {2,5,9,26,34};
+    Create(A, 5);
+    Create2(B,5);
+    
+    Merge(first,second);
 
+    Display(third);
 
-// Node* temp=Search(first,35);
-// if(temp)
-//     cout<<"Key "<<temp->Data<< " is found at address "<<temp<<endl;
-// else
-//     cout<<"Key not found "<<endl;
-//cout<<"After Inserting:\n";
-Insert(1, 12);
-
-Display(first);
-
-return 0;
-
+    return 0;
 }
